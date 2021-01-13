486b45d883d6871dca3b1e784f4930e4c48b7134
Zavizhenetc <zavizhenetc@gmail.com>
2021-01-13 17:46:07 +0300

    Updates

---
+++ precache-manifest.0ff24051d5b4d5aab014a6ae063ead1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6821ac067aee1f9ca94cb7390c7d25",
+    "url": "/netWork/index.html"
+  },
+  {
+    "revision": "65aebe04896f510994e0",
+    "url": "/netWork/static/css/main.7b87cda3.chunk.css"
+  },
+  {
+    "revision": "3af4a1f563eebd0a6fbc",
+    "url": "/netWork/static/js/2.51319af4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netWork/static/js/2.51319af4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65aebe04896f510994e0",
+    "url": "/netWork/static/js/main.35d20aee.chunk.js"
+  },
+  {
+    "revision": "70bc5ce5b39b29969bad",
+    "url": "/netWork/static/js/runtime-main.2ebf369e.js"
+  },
+  {
+    "revision": "8c1b0f376b85a3e47def1964e2edbbdb",
+    "url": "/netWork/static/media/preloader.8c1b0f37.gif"
+  }
+]);
