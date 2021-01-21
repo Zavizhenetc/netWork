28685823320228a1ccc81edc34deaa3e34caa344
Zavizhenetc <zavizhenetc@gmail.com>
2021-01-21 18:52:23 +0300

    shouldComponentUpdate, React.memo

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -1,34 +1,36 @@
-import React, {Component} from 'react';
+import React from 'react';
 import styles from './Posts.module.css';
 import Post from './Post/Post.jsx';
 import {Field, reduxForm} from "redux-form";
-import { maxLengthCreator, required} from "../../../utils/validators/validators";
+import {maxLengthCreator, required} from "../../../utils/validators/validators";
 import {Textarea} from "../../common/FormsControls/FormsControls";
 
 
-const Posts = (props) => {
+const  Posts= React.memo(props=> {
+  console.log('test')
   const postsElements = props.posts.map((post) => (
     <Post message={post.message} likesCount={post.likesCount} key={post.id}/>
   ));
   const onAddPost = (values) => {
     props.addPost(values.newPostText);
   };
-const maxLength10 = maxLengthCreator(10);
-  const AddNewPostForm=(props)=>{ 
-    return(
-      <form className={styles.input} onSubmit={props.handleSubmit} >
-        <Field className={styles.addPost} name='newPostText'  placeholder='что у вас нового' component={Textarea}
-               validate={[required, maxLength10 ]}  />
-        <button className={styles.button} >Add post</button>
+  const maxLength10 = maxLengthCreator(10);
+  const AddNewPostForm = (props) => {
+    return (
+      <form className={styles.input} onSubmit={props.handleSubmit}>
+        <Field className={styles.addPost} name='newPostText' placeholder='что у вас нового' component={Textarea}
+               validate={[required, maxLength10]}/>
+        <button className={styles.button}>Add post</button>
       </form>
     )
   }
-  const AddNewPostFormRedux = reduxForm({form:'ProfileAddNewPostForm'})(AddNewPostForm);
+  const AddNewPostFormRedux = reduxForm({form: 'ProfileAddNewPostForm'})(AddNewPostForm);
   return (
     <div className={styles.posts}>
-<AddNewPostFormRedux  onSubmit={onAddPost} />
+      <AddNewPostFormRedux onSubmit={onAddPost}/>
       {postsElements}
     </div>
   );
-};
+});
+
 export default Posts;
